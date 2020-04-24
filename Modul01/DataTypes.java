fda034f15d15f5850eb1510317ed4ae660a06bdf
Sandi <sandi.uniska@gmail.com>
2020-04-24 15:53:51 +0800

    Input dengan Scanner

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
+
+  Scanner scanner = new Scanner(System.in);
   
   String namaDepan = "Sandi";
   String namaTengah = "Sandi";
@@ -12,11 +15,24 @@ public class DataTypes{
   char nilaiAbjad = 'A';
   boolean tampan = true;
 
+  System.out.print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out.print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.print("Tampan ? : ");
+  tampan = scanner.nextBoolean();
+
+  System.out.println("================= OUTPUT ====================");
   System.out.println("Nama Depan    : "+namaDepan);
   System.out.println("Nama Tengah   : "+namaTengah);
   System.out.println("Nama Belakang : "+namaBelakang);
   System.out.println("Usia          : "+usia);
-  System.out.println("Target Kuliah : "+targetTahunKuliah + "tahun");
+  System.out.println("Target Kuliah : "+ targetTahunKuliah + "tahun");
   System.out.println("IPK           : "+ipk);
   System.out.println("Nilai PBO     : "+nilaiAbjad);
   System.err.println("Tampan        : "+tampan);
