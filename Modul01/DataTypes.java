6893f4817fc77ab8098cc6ac3a571ff48c414cfb
Sandi <sandi.uniska@gmail.com>
2020-04-24 15:20:36 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+  
+  String namaDepan = "Sandi";
+  String namaTengah = "Sandi";
+  String namaBelakang = "Sandi";
+  int usia = 26;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
