e5ce1bdd1f88ce2cd94cbd2911bbec724691aa1e
Sandi <sandi.uniska@gmail.com>
2020-04-24 16:40:59 +0800

    Operasi Tipe String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -16,5 +16,23 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("Sa", "ndi"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
+
     }
 }
